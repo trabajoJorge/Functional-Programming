a1d77dbdd9ffd3945160055d678f87f9339fc424
Jorge Iglesias <trabajo.jorge2000@gmail.com>
2021-11-07 19:33:05 +0100

    Practica 1.1 - Falta un detalle

--- Practica1.1/usuarioCalendario.hs
+++ Practica1.1/usuarioCalendario.hs
@@ -1,3 +1,4 @@
+
 import Calendarios(printCalendario)
 
 stringToInt :: String -> Int
@@ -5,14 +6,18 @@ stringToInt s= read s :: Int
 
 printCalendarioUsuario :: IO ()
 printCalendarioUsuario= do
-                            print "Introduce el numero de columnas que quieras en tu calendario (3 o 4): "
-                            c1 <- getLine
-                            let n1= stringToInt c1
-                            if (n1>4 || n1<3) then error("El numero introduccido no esta dentro del rango")
-                            else do 
-                                    print "Introduce el año del calendario que quieras imprimir: "
-                                    c2 <- getLine 
-                                    let n2= stringToInt c2
-                                    if (n2<1000) then error ("El dibujo no es correcto") --Al no estar corregida la anterior practica no puedo imprimir años anteriores al año 1000
-                                    else do
-                                            printCalendario n1 n2
+                                print "Introduce el numero de columnas que quieras en tu calendario (3 o 4): "
+                                c1 <- getLine
+                                let n1= stringToInt c1
+                                if (n1>4 || n1<3) then 
+                                        --print ("El numero introduccido no esta dentro del rango")
+                                        printCalendarioUsuario
+                                else do 
+                                        print "Introduce el año del calendario que quieras imprimir (1000<=año<=9999): "
+                                        c2 <- getLine 
+                                        let n2= stringToInt c2
+                                        if (n2<1000 || n2>9999) then 
+                                                --print ("El dibujo no es correcto") 
+                                                printCalendarioUsuario
+                                        else do
+                                                printCalendario n1 n2
