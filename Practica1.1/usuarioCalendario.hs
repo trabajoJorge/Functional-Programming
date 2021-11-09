a831b760b09b99e61f679c84819030d82683051b
Jorge Iglesias <trabajo.jorge2000@gmail.com>
2021-11-09 20:55:46 +0100

    Pracrica 1.1 - Finalizada

--- Practica1.1/usuarioCalendario.hs
+++ Practica1.1/usuarioCalendario.hs
@@ -1,35 +1,36 @@
 
 import Calendarios(printCalendario)
 
+-- NOTA: La letra ñ ha sido sustituida por n. Cualquier palabra malsonante es pura coincidencia.
+
 stringToInt :: String -> Int
 stringToInt s= read s :: Int
 
-printCalendarioUsuario :: IO ()
-printCalendarioUsuario= do
-                                print "Introduce el numero de columnas que quieras en tu calendario (3 o 4): "
-                                c1 <- getLine
-                                let n1= stringToInt c1
-                                if (n1>4 || n1<3) then do
-                                        print ("El numero introduccido no esta dentro del rango")
-                                        printCalendarioUsuario
-                                else do 
-                                        print "Introduce el ano del calendario que quieras imprimir (1000<=ano<=9999): "
-                                        c2 <- getLine 
-                                        let n2= stringToInt c2
-                                        if (n2<1000 || n2>9999) then do
-                                                print ("El ano no es correcto") 
-                                                printCalendarioUsuario2 n1
-                                        else do
-                                                printCalendario n1 n2
+main :: IO ()
+main= do
+        print "Introduce el numero de columnas que quieras en tu calendario (3 o 4): "
+        c1 <- getLine
+        let n1= stringToInt c1  
+        if (n1>4 || n1<3) then do
+        print ("El numero introduccido no esta dentro del rango")
+        main
+        else do 
+        print "Introduce el ano del calendario que quieras imprimir (1000<=ano<=9999): "
+        c2 <- getLine 
+        let n2= stringToInt c2
+        if (n2<1000 || n2>9999) then do
+                print ("El ano no es correcto") 
+                printCalendarioUsuario n1
+        else do
+                printCalendario n1 n2
 
---printCalendarioUsuario2 :: IO ()
-printCalendarioUsuario2 :: Int -> IO ()
-printCalendarioUsuario2 col= do
+printCalendarioUsuario :: Int -> IO ()
+printCalendarioUsuario col= do
                                 print "Introduce el ano del calendario que quieras imprimir (1000<=ano<=9999): "
                                 c2 <- getLine 
                                 let n2= stringToInt c2
                                 if (n2<1000 || n2>9999) then do
                                         print ("El ano no es correcto") 
-                                        printCalendarioUsuario2 col
+                                        printCalendarioUsuario col
                                 else do
                                         printCalendario col n2
