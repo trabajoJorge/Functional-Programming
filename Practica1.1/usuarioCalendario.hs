6186167945d6a478e47b7b3fe00db38eca06861f
Jorge Iglesias <trabajo.jorge2000@gmail.com>
2021-11-09 19:57:57 +0100

    Practica 1.1 - Finalizada

--- Practica1.1/usuarioCalendario.hs
+++ Practica1.1/usuarioCalendario.hs
@@ -9,15 +9,27 @@ printCalendarioUsuario= do
                                 print "Introduce el numero de columnas que quieras en tu calendario (3 o 4): "
                                 c1 <- getLine
                                 let n1= stringToInt c1
-                                if (n1>4 || n1<3) then 
-                                        --print ("El numero introduccido no esta dentro del rango")
+                                if (n1>4 || n1<3) then do
+                                        print ("El numero introduccido no esta dentro del rango")
                                         printCalendarioUsuario
                                 else do 
-                                        print "Introduce el año del calendario que quieras imprimir (1000<=año<=9999): "
+                                        print "Introduce el ano del calendario que quieras imprimir (1000<=ano<=9999): "
                                         c2 <- getLine 
                                         let n2= stringToInt c2
-                                        if (n2<1000 || n2>9999) then 
-                                                --print ("El dibujo no es correcto") 
-                                                printCalendarioUsuario
+                                        if (n2<1000 || n2>9999) then do
+                                                print ("El ano no es correcto") 
+                                                printCalendarioUsuario2 n1
                                         else do
                                                 printCalendario n1 n2
+
+--printCalendarioUsuario2 :: IO ()
+printCalendarioUsuario2 :: Int -> IO ()
+printCalendarioUsuario2 col= do
+                                print "Introduce el ano del calendario que quieras imprimir (1000<=ano<=9999): "
+                                c2 <- getLine 
+                                let n2= stringToInt c2
+                                if (n2<1000 || n2>9999) then do
+                                        print ("El ano no es correcto") 
+                                        printCalendarioUsuario2 col
+                                else do
+                                        printCalendario col n2
